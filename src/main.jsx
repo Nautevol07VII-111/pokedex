a12c719a6396fbcd2bf971f94cf05f5e50a84e1d
Nile Jackson <nilejack@udel.edu>
2025-03-22 19:51:18 -0400

    adds/fix(node/vite versioning issues): proper configs for initial setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './App.css'
 import App from './App.jsx'
+import './fanta.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <h1>Nile</h1>
     <App />
   </StrictMode>,
 )
